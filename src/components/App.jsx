50fdb174de2ad1881cf5be6be52c66538018a5a5
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-13 20:45:02 +0100

    add component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import Feedback from './Feedback/Feedback';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Feedback />
     </div>
   );
 };
